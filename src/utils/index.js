9171b249b0e612f5487c6d160f7cb2a5e6c4799a
Pierre Cabrière <hello@pierrecabriere.fr>
2021-09-17 09:26:07 +0200

    add axios export

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import { getTokenFromCookie, setTokenFromCookie } from './cookies'
 import { authmanager, logout } from './authmanager'
+import axios from './axios'
 import ReactEntry from './ReactEntry'
 
 export {
   authmanager,
+  axios,
   logout,
   getTokenFromCookie,
   setTokenFromCookie,
